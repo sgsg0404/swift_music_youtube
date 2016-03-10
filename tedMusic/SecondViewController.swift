981910a2c2511677bcde723c55494a605cb9dc07
ted <ted@teds-mac.local>
2016-03-10 13:18:27 +0800

    Initial Commit

---
+++ tedMusic/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tedMusic
+//
+//  Created by ted on 10/3/16.
+//  Copyright © 2016 ted. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
